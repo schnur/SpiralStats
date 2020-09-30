249537216bd65e055c76024a516a76b99d30439b
Ryan <ryanschnurrpi@gmail.com>
2020-09-29 21:46:38 -0400

    print all teams, there scores and who is active

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-    #return "Hia"
     #swid: "{5219793E-A1E1-409D-B4F6-4553002DE3C5}"
     #espn2 AECFdZN2ME9jC0RpPvn0iFSucTA6zC2Aci1594ItlyXeW7OarK8YC1p2qWoATmAVYklBskhYNcfrAc%2FCeVsB7T2wesET%2BYRZfOE7e9mJeQJoXNzdZsGjMfJ13pN1uRMnhYT%2FWrWkdWY%2F7GX9HiO%2BWzDHiAF8zLhB68PyvqN0LjzeFtZYhBbOfDNMsyi4rUVXZGjdq%2FqxpjbJF%2F4Sj1Y0dUJ2bW6NInbZem2HoEGYKHpK8gV5hSbZjNFAUTg3L4hqqNQijChIF7Rd0PF4Ogrh0b1q
     leagueID="21251"
@@ -23,28 +22,57 @@ def home():
     #mTeam what moves were made
     #mRoster news
     #mBoxscore complex but maybe can get the data from here
+
+
+    #general league info
+    r=requests.get(url,cookies={"swid": swid_cookie,"espn_s2": espn2_cookie})
+   
+    d = r.json()
+    
+    weekNumber=int(d["scoringPeriodId"]) #what week
+    idToTeamName={}
+    for x in d["teams"]:
+        idToTeamName[x["id"]]=x["location"]+" "+x["nickname"]
+    #print(idToTeamName)
+
+
+
     r=requests.get(url,cookies={"swid": swid_cookie,"espn_s2": espn2_cookie}, params={"view": "mMatchup"})
    
     d = r.json()
     
-    #for i in d:
-    #    print(i)
-    #print(d["schedule"])
-    #for i in d["schedule"]:
-    #    print(i)
-    #for i in d["schedule"][12]:
-    #    print(i)
-    '''
-    away
-    home
-    id
-    matchupPeriodId
-    winner
-    '''
-    #print(d["schedule"][12]["home"]["rosterForCurrentScoringPeriod"]["entries"])
-    #for i in d["schedule"][12]["home"]["rosterForCurrentScoringPeriod"]["entries"]:
-    #    print(i["playerPoolEntry"]["player"]["fullName"])
+
+    for j in range(int(len(idToTeamName)/2)):
+        print(idToTeamName[d["schedule"][(weekNumber-1)*int(len(idToTeamName)/2)+j]["home"]["rosterForCurrentScoringPeriod"]["entries"][0]["playerPoolEntry"]["onTeamId"]])
+        totalHome=0
+        for i in d["schedule"][(weekNumber-1)*int(len(idToTeamName)/2)+j]["home"]["rosterForCurrentScoringPeriod"]["entries"]:
+            print(i["playerPoolEntry"]["player"]["fullName"], end="")
+            print("  "+str(i["playerPoolEntry"]["appliedStatTotal"]), end="")
+            if(i["lineupSlotId"]!=20):
+                totalHome+=i["playerPoolEntry"]["appliedStatTotal"]
+                print(" active")
+            else:
+                print()
+        print("total "+str(totalHome))
+        print()
+        print()
+        totalAway=0
+        print(idToTeamName[d["schedule"][(weekNumber-1)*int(len(idToTeamName)/2)+j]["away"]["rosterForCurrentScoringPeriod"]["entries"][0]["playerPoolEntry"]["onTeamId"]])
+        for i in d["schedule"][(weekNumber-1)*int(len(idToTeamName)/2)+j]["away"]["rosterForCurrentScoringPeriod"]["entries"]:
+            print(i["playerPoolEntry"]["player"]["fullName"], end="")
+            print("  "+str(i["playerPoolEntry"]["appliedStatTotal"]), end="")
+            if(i["lineupSlotId"]!=20):
+                totalAway+=i["playerPoolEntry"]["appliedStatTotal"]
+                print(" active")
+            else:
+                print()
+        print("total "+str(totalAway))
+        print()
+        print()
+
+
     #0-11 are nothing prob week data
+    return(d)
     '''
     mMatchup
     draftDetail-nope
@@ -57,15 +85,9 @@ def home():
     status - nitch info
     teams
     '''
-    return(d)
+
+
   
   #what we need for each player
     #name, points, team/
 
-  #jobs
-  #espn data-ryan
-  #nfl data-Arya
-  # dream team -shamar
-  # find out how to use cookies -colton
-  
-  # testing testing
