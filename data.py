b2470f416925c774a1d1f206e176d11d60ecc9fc
Shamar Samuels <shamarsamuelsbusiness@gmail.com>
2020-10-10 18:22:11 -0400

    Data for Waived Players

--- data.py
+++ data.py
@@ -13,13 +13,31 @@ with open('positions.json', 'r') as positions_file:
 
 with open('free_agents.json', 'r') as free_agents_file:
     free_agents = json.load(free_agents_file)
+    
+with open('waivers.json', 'r') as waivers_file:
+    waivers = json.load(waivers_file)
 
 with open('free_agent_details.json', 'r') as free_agent_details_file:
     free_agent_details = json.load(free_agent_details_file)
 
+# for position in positions:
+#     if position in free_agents:
+#         for player in free_agents[position]:
+#             if not (player["name"] in free_agent_details):
+#                 try:
+#                     agent_details = lg.player_details(player["name"])
+#                     free_agent_details[player["name"]] = agent_details[0]
+#                 except RuntimeError:
+#                     print(player["name"], 'was not found.')
+#                 with open('free_agent_details.json', 'w') as free_agent_details_file:
+#                     json.dump(free_agent_details, free_agent_details_file)
+#             else:
+#                 print(player["name"], "already on the list.")
+
+
 for position in positions:
-    if position in free_agents:
-        for player in free_agents[position]:
+    if position in waivers:
+        for player in waivers[position]:
             if not (player["name"] in free_agent_details):
                 try:
                     agent_details = lg.player_details(player["name"])
@@ -30,6 +48,7 @@ for position in positions:
                     json.dump(free_agent_details, free_agent_details_file)
             else:
                 print(player["name"], "already on the list.")
+
 
 
 
