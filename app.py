fe79960e0d0173639fde7b2aa16a9dbd963df834
Shamar Samuels <shamarsamuelsbusiness@gmail.com>
2020-10-13 21:38:47 -0400

    Added Files

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+import json
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html') 
+
+@app.route('/signup')
+def players():
+    return render_template('signup.html') 
+    
+@app.route('/login')
+def players():
+    return render_template('login.html') 
+
+if __name__ == '__main__':
+    app.run()
