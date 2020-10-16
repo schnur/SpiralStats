77a6508b75d910d39389be999721b4617e45fbca
Shamar Samuels <shamarsamuelsbusiness@gmail.com>
2020-10-15 22:56:06 -0400

    Adding Post Routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, render_template,url_for, request
+from flask import Flask, render_template,url_for, request, redirect
 from forms import SignupForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'aa0eb2fcfae25481525f3fe11871c26a'
@@ -7,15 +7,36 @@ app.config['SECRET_KEY'] = 'aa0eb2fcfae25481525f3fe11871c26a'
 def main():
     return render_template('index.html') 
 
-@app.route('/signup')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/signup', methods = ['POST', 'GET'])
 def signup():
-    form = SignupForm()
-    return render_template('signup.html', title = 'Sign Up', form = form) 
+    if request.method == "GET":
+        form = SignupForm()
+        return render_template('signup.html', title = 'Sign Up', form = form) 
+    elif request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+        print("Username :", username)
+        print("Passwprd :", password)
+        print("Confirm Password :", confirm_password)
+        return redirect(url_for('home'))
     
-@app.route('/login')
+@app.route('/login', methods = ['POST', 'GET'])
 def login():
-    form = LoginForm()
-    return render_template('login.html',title = 'Log In', form = form) 
+    if request.method == "GET":
+        form = LoginForm()
+        return render_template('login.html',title = 'Log In', form = form) 
+    elif request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        print("Username :", username)
+        print("Passwprd :", password)
+        return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run()
