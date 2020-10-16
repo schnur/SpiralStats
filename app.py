c8f3cf1ff604dcd97212144268b55e6f17b48b6f
Aryash <aryashahvar14@gmail.com>
2020-10-15 22:35:02 -0400

    updates

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template, request
 import json
+from flask import Flask, render_template,url_for, request
+from forms import SignupForm, LoginForm
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'aa0eb2fcfae25481525f3fe11871c26a'
 @app.route('/')
 def main():
     return render_template('index.html') 
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html') 
+    form = SignupForm()
+    return render_template('signup.html', title = 'Sign Up', form = form) 
     
 @app.route('/login')
 def login():
-    return render_template('login.html') 
+    form = LoginForm()
+    return render_template('login.html',title = 'Log In', form = form) 
 
 if __name__ == '__main__':
     app.run()
