d50851695ac8f28fc4a693d4fcb18a8729d32696
Aryash <aryashahvar14@gmail.com>
2020-10-20 21:17:01 -0400

    Updating non zero season stats for each player

--- app.py
+++ app.py
@@ -14,7 +14,25 @@ with open('waivers.json', 'r') as waivers_file:
 with open('free_agent_details.json', 'r') as free_agent_details_file:
     free_agent_details = json.load(free_agent_details_file)
 
+with open('season_stats.json', 'r') as season_stats_file:
+        season_stats = json.load(season_stats_file)
 
+stats_words = {
+    "Pass Yds": 'Passing Yards', 
+    "Pass TD": 'Passing Touchdowns', 
+    "Int": 'Interception', 
+    "Rush Att": 'Rush Attempts', 
+    "Rush Yds": 'Rush Yards', 
+    "Rush TD": 'Rush Touchdown', 
+    "Rec": 'Reception', 
+    "Rec Yds": 'Recieving Yards', 
+    "Rec TD": 'Recieving Touchdowns', 
+    "Ret TD": 'Return Touchdowns', 
+    "2-PT": '2 Point Convertion', 
+    "Fum Lost": 'Fumble Lost', 
+    "Fum Ret TD": 'Fumble Return Touchdowns', 
+    "Targets": 'Targets'
+}
 @app.route('/')
 def main():
     return render_template('players.html', positions=positions, free_agents = free_agents, free_agent_details = free_agent_details, waivers=waivers) 
@@ -23,8 +41,9 @@ def main():
 def players():
     player_name = request.args.get('player_name')
     player = free_agent_details[player_name]
-    if player:
-        return render_template('player.html', player = player)
+    stats = season_stats[player_name]
+    if player and stats:
+        return render_template('player.html', player = player, stats = stats, stats_words = stats_words)
     else:
         return "Player Data Not Found"
     
